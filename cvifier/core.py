4dc8f29ea4242e08b9aff0100b4f24da194c997b
Erik Tollerud <erik.tollerud@gmail.com>
2013-10-24 18:23:11 -0400

    add back in clever parser

--- cvifier/core.py
+++ cvifier/core.py
@@ -58,11 +58,82 @@ def extract_contact_info(doctree):
     return cidict
 
 
-def load_settings(fn):
+def load_settings(content, comment='#'):
     """
-    loads settings from filename `fn`, and return them as a dict
-    """
+    Parses a settings file.
 
+    The file itself should look like::
+
+      [documentclass]
+      article
+
+      [documentoptions]
+      12pt
+
+      [latex_preamble]
+      \usepackage{hyperref}
+      \usepackage{color}
+
+
+
+    Parameters
+    ----------
+    content : str, file-like object, or list of strings
+        The file (or list of lines) that should be parsed assuming the
+        format described above.
+    comment : str or None
+        The character sequence to interpret as marking the start of a
+        comment.  If a '\\' comes before, will be treated as a regular
+        character.
+    Returns
+    -------
+    contentdict : collections.OrderedDict
+        A dictionary mapping section names to section content.
+        Preserves the order the file provides them in.
+
+    """
+    import re
+    from collections import defaultdict, OrderedDict
+
+    markerrex = re.compile(r'\[(.*?)\](.*)')
+    commentre = re.compile(r'(.*?)((?<!\\){0})'.format(comment))
+    esccomment = '\\' + comment
+
+    if isinstance(content, basestring):
+        with open(content, 'r') as f:
+            lines = f.read().split('\n')
+    elif hasattr(content, 'read'):
+        lines = content.read().split('\n')
+    else:
+        lines = []
+        for l in content:
+            lines.append(str(l))
+
+    seccontent = defaultdict(list)
+    orderedsections = []
+    insection = None
+    for l in lines:
+        #
+        mtch = commentre.match(l)
+        if mtch is not None:
+            l = mtch.group(1)
+            if l == '':
+                # pure comment line
+                continue
+        l.replace(esccomment, comment)
+
+        mtch = markerrex.match(l)
+
+        if mtch is None:
+            if insection:
+                seccontent[insection].append(l)
+        elif mtch.group(2).strip() != '':
+            raise ValueError('Content had a line that looks like a section header, but with extra content:"{0}"'.format(l))
+        else:
+            insection = mtch.group(1)
+            orderedsections.append(insection)
+
+    return OrderedDict([(k, '\n'.join(seccontent[k]).rstrip()) for k in orderedsections])
 
 
 def write_doctree(doctree, writer, fout=None, settingsfile=None):
@@ -83,8 +154,6 @@ def write_doctree(doctree, writer, fout=None, settingsfile=None):
     else:
         stgs = load_settings(writer.supported[0] + '.cvsettings')
         res = publish_from_doctree(doctree, writer=writer, settings_overrides=stgs)
-
-
 
     if fout:
         if hasattr(fout, 'write'):
